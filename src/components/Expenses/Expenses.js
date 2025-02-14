f117c3f90b55cca3cf1a9b555b993a283618cbed
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2025-02-14 12:14:03 +0200

    Update Expenses.js

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -32,36 +32,6 @@ const Expenses = (props) => {
           onChangeFilter={filterChangeHandler}
         />
         {expensesContent}
-        {/*  //* NO LONGER NEEDED */}
-        {/* {filteredExpenses.length === 0 && <p>No Expenses Found</p>}
-        {filteredExpenses.length > 0 &&
-          filteredExpenses.map(({ title, amount, date, id }) => {
-            return (
-              <ExpenseItem title={title} amount={amount} date={date} key={id} />
-            );
-          })} */}
-
-        {/*  //* NO LONGER NEEDED */}
-        {/* <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
-        />
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        />
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        /> */}
       </Card>
     </div>
   );
